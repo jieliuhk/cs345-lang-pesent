e7d536ee94b5af572a4462593560eccf622f6e1e
jie_liu <guygor.jie@gmail.com>
2021-05-12 19:56:33 -0700

    concurrence

--- concurrent/findprime/main.go
+++ concurrent/findprime/main.go
@@ -2,7 +2,22 @@ package main
 import (
    "fmt"
    "github.com/jieliuhk/cs345-lang-pesent/oo/mytype"
+   "sync"
 )
+
+func printPrime(num int, wg *sync.WaitGroup){
+   defer wg.Done()
+
+   myint := mytype.MyInt{
+     Value: num,
+   }
+
+   isPrime := myint.IsPrime()
+
+   if isPrime {
+      fmt.Printf("%d ", num)
+   }
+}
 
 func printPrimeNumbers(num1, num2 int){
    if num1<2 || num2<2{
@@ -10,18 +25,16 @@ func printPrimeNumbers(num1, num2 int){
       return
    }
 
+   var wg sync.WaitGroup
+
    for num1 <= num2 {
-       myint := mytype.MyInt{
-        Value: num1,
-       }
-      isPrime := myint.IsPrime()
-
-      if isPrime {
-         fmt.Printf("%d ", num1)
-      }
-
+      wg.Add(1)
+      printPrime(num1, &wg)
       num1++
    }
+
+   wg.Wait()
+
    fmt.Println()
 }
 
